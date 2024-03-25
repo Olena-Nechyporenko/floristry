f1467ed507dd67f67322c88c36bd94cbc9b03f07
Olena-Nechyporenko <127235568+Olena-Nechyporenko@users.noreply.github.com>
2024-03-25 10:55:46 +0000

    Deploying to gh-pages from @ Olena-Nechyporenko/floristry@8aaae6ebfb743385f14fa1d906f2103000bcc1aa 🚀

---
+++ static/js/680.b82dc846.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[680],{680:function(e,t,n){n.r(t),n.d(t,{default:function(){return c}});var r=n(184);function c(){return(0,r.jsx)("section",{children:"Home Page"})}}}]);
+//# sourceMappingURL=680.b82dc846.chunk.js.map

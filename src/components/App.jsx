eb5748f0381a7cb05eb2712277259a171a8e0726
Nataly-Naf <nat.spivak84@gmail.com>
2024-03-27 09:58:46 +0000

    Try to add backend

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,6 +3,7 @@ import { Route, Routes } from 'react-router-dom';
 import { Layout } from './Layout/Layout';
 import NotFoundPage from 'pages/NotFoundPage/NotFoundPage';
 import CatalogPage from 'pages/CatalogPage/CatalogPage';
+import CartPage from 'pages/CartPage/CartPage';
 
 const HomePage = lazy(() => import('pages/HomePage/HomePage'));
 
@@ -13,6 +14,7 @@ export const App = () => {
         <Route index element={<HomePage />} />
         <Route path="*" element={<NotFoundPage />} />
         <Route path="/catalog" element={<CatalogPage />} />
+        <Route path="/cart" element={<CartPage />} />
       </Route>
     </Routes>
   );
--- src/components/Catalog/Catalog.jsx
+++ src/components/Catalog/Catalog.jsx
@@ -1,9 +1,20 @@
 import { BouquetItem } from 'components/BouquetItem/BouquetItem';
 import { CatalogWrapper } from './Catalog.styled';
-import bouquets from './bouquets.json';
+// import bouquets from './bouquets.json';
 import { nanoid } from 'nanoid';
+import { useEffect } from 'react';
+import { fetchBouquets } from 'redux/bouquets/operations';
+import { selectAllBouquets } from 'redux/bouquets/selectors';
+import { useSelector } from 'react-redux';
 
 export const Catalog = () => {
+  useEffect(() => {
+    fetchBouquets();
+  }, []);
+
+  const bouquets = useSelector(selectAllBouquets);
+  console.log(bouquets);
+
   return (
     <CatalogWrapper>
       {bouquets.map(bouquet => (
